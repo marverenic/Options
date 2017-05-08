8759c82a2df0501efeec8647b6a02859e50b09fc
Andrew Bailey <marverenic@gmail.com>
2017-05-07 22:53:33 -0400

    Create initial demo application

--- demo/src/main/java/com/marverenic/options/demo/MainActivity.java
+++ demo/src/main/java/com/marverenic/options/demo/MainActivity.java
@@ -1,7 +1,16 @@
 package com.marverenic.options.demo;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+
+import com.marverenic.options.Option;
+import com.marverenic.options.OptionFragment;
+import com.marverenic.options.types.sharedpreferences.SharedPreferenceSwitchOption;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.preference_container, new PreferenceFragment())
+                .commit();
+    }
+
+    public static class PreferenceFragment extends OptionFragment {
+
+        @Override
+        public List<Option> createOptionList() {
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+
+            return Arrays.asList(new Option[] {
+                    new SharedPreferenceSwitchOption.Builder()
+                            .setSharedPreferences(prefs)
+                            .setKey("switch-1")
+                            .setTitle("Switch 1")
+                            .setEnabledDescription("Switch 1 is enabled")
+                            .setDisabledDescription("Switch 1 is disabled")
+                            .build(),
+            });
+        }
     }
 }
